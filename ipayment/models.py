fa95cf7f950b20c0266b4dc782d5acfb6f873bdf
Jacob Rief <jacob.rief@gmail.com>
2012-05-23 10:08:54 +0200

    Added verbose names to ipayment db model

--- ipayment/models.py
+++ ipayment/models.py
@@ -10,14 +10,18 @@ class Confirmation(models.Model):
     class Meta:
         verbose_name = _('IPayment Confirmation')
 
-    shopper_id = models.IntegerField()
-    vendor_comment = models.TextField(null=True, blank=True)
-    ret_booknr = models.CharField(max_length=63, )
+    shopper_id = models.IntegerField(
+        verbose_name=_('Unique identifier for submitted payments'))
+    vendor_comment = models.TextField(null=True, blank=True,
+        verbose_name=_('Additional comments from the vendor'))
+    ret_booknr = models.CharField(max_length=63,
+        verbose_name=_('IPayments internal booking number'))
     ret_errorcode = models.IntegerField()
     trx_paymentmethod = models.CharField(max_length=63)
     ret_trx_number = models.CharField(max_length=63)
     ret_transdatetime = models.DateTimeField()
-    ret_ip = models.IPAddressField()
+    ret_ip = models.IPAddressField(
+        verbose_name=_('The clients IP address'))
     trx_typ = models.CharField(max_length=63)
     addr_name = models.CharField(max_length=63, verbose_name='Cardholder name')
     trx_amount = CurrencyField()
